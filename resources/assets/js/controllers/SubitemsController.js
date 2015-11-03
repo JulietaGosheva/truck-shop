7d3f5fe1332984559848b46c16cffd06235e4d99
RaISug <radoslav.i.sugarev@gmail.com>
2015-11-04 00:16:35 +0200

    Remove subitems templates and controllers
    Change-Id: I0e9d9f72bf5e0756549d9d68b8f54921f64c961c
    Signed-off-by: RaISug <radoslav.i.sugarev@gmail.com>

--- resources/assets/js/controllers/SubitemsController.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var module = angular.module("AngularApplication");
-	
-	var SubitemsController = function($scope, $routeParams, NavigationUtil) {
-//		$scope.subitems = NavigationUtil.getNavigationSubItems($routeParams.navItem);
-	};
-	
-	module.controller("SubitemsController", ["$scope", "$routeParams", "NavigationUtil", SubitemsController]);
-})();
