7d3d24288bba4d0e67cd1da28a3210d1b516d70b
RaISug <radoslav.i.sugarev@gmail.com>
2016-02-15 23:08:03 +0200

    Fix migration scripts bug
    Change-Id: I8c71dbdef40e059a8d923cccc8a924dd51e01c05
    Signed-off-by: RaISug <radoslav.i.sugarev@gmail.com>

--- database/migrations/2015_11_23_212302_users.php
+++ database/migrations/2015_11_23_212302_users.php
@@ -15,9 +15,9 @@ class Users extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('email', 64);
+            $table->string('password', 64);
             $table->string('first_name', 32);
             $table->string('last_name', 32);
-            $table->integer('role');
         });
     }
 
