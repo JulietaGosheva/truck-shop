88472380a426aa4048fd248b12b014bbe041a399
RaISug <radoslav.i.sugarev@gmail.com>
2016-03-04 15:45:06 +0200

    Add more responsive UI support
    Change-Id: I5bfcdbab87a4c6ee45188b94909697a1cc7a2979
    Signed-off-by: RaISug <radoslav.i.sugarev@gmail.com>

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,6 +2,8 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+		<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+		
 		<title>Авточасти</title>
 		
 		<script src="resources/dependencies/js/jquery-2.1.3.min.js"></script>
